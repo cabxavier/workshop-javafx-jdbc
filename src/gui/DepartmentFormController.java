e4638bd6fa8a9dbc8d8a4918d8bb9a80bfdf7d5c
cabxavier <cesaraugustobarbosaxavier@gmail.com>
2022-07-10 16:46:08 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import javafx.event.ActionEvent;
@@ -21,6 +24,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
 	@FXML
 	private TextField txtId;
@@ -44,6 +49,10 @@ public class DepartmentFormController implements Initializable {
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
 
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
@@ -60,11 +69,17 @@ public class DepartmentFormController implements Initializable {
 			
 			entity = getFormData();
 			service.saveOrUpdate(entity);
-			
+			notifyDataChangeListeners();
 			gui.util.Utils.currentStage(event).close();
 
 		} catch (DbException e) {
 			Alerts.showAlert("Erro save object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
 		}
 	}
 
